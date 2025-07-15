8b1c6a0191e69119cde2c484214744a19a7a9d92
Miguel92 <joelmiguelvalente@gmail.com>
2025-07-15 15:12:13 -0300

    [chore] Cambios en package.json y vite.config.js para uso con Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/PachambaKids-React-App/",
+	/**
+	 * Descomentar línea 11 su usas GH-PAGES
+	 * y borras línea 12
+	 */
+	// base: "/PachambaKids-React-App/",
+	base: "/",
 });
